62fc5d88d2d015c08c2e37495adf652366cd47b2
alumno <alumno@pc-candelap.uacj.mx>
2024-11-11 10:44:52 -0700

    Con Api

---
+++ Desktop/MonsterHigh/MonsterHigh/ViewCell/MonsterCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MonsterCollectionViewCell.swift
+//  MonsterHigh
+//
+//  Created by alumno on 11/11/24.
+//
+
+import UIKit
+
+class MonsterCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var foto_de_perfil: UIImageView!
+    @IBOutlet weak var Nombre: UILabel!
+    @IBOutlet weak var Padres: UILabel!
+}
